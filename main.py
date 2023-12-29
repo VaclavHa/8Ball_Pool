9a60a7311d6e9ac9eecf652117cca6ef5babb0d4
VaclavHa <hanzlvaclav00@gmail.com>
2023-12-29 17:54:11 +0100

    Editing printing tables

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 
+
 def create_table(width: int, height: int):
     table = []
     for _ in range(height):  # loop to create lists A.K.A rows
@@ -15,6 +16,7 @@ def place_ball(table: list, x: int, y: int):
     if x >= 0 and y >= 0 and x < len(table[0]) and y < len(table):  # check if coordinates are within the table
         table[y][x] = "O"
 
+        print("\nBall has been placed \n")
         for row in table:
             print(' '.join(row))
         print("\n")
@@ -43,6 +45,7 @@ def move_ball(placed_ball: list, start: tuple, direction: str, steps: int):
     width = len(placed_ball[0])  # number of columns
 
     for step in range(steps + 1):
+        print(f"Step: {step}")
 
         if step != 0:
             placed_ball[y][x] = str(step)
@@ -54,32 +57,27 @@ def move_ball(placed_ball: list, start: tuple, direction: str, steps: int):
         next_x = x + dx
         next_y = y + dy
 
-
-        if next_x < 0 or next_x >= width: # to check borders
+        if next_x < 0 or next_x >= width:  # to check borders
             dx = -dx
             next_x = x + dx
 
-        if next_y < 0 or next_y >= height: # to check borders
+        if next_y < 0 or next_y >= height:  # to check borders
             dy = -dy
             next_y = y + dy
 
-
-
-        x, y = next_x, next_y # new/last position of the ball
+        x, y = next_x, next_y  # new/last position of the ball
 
     return print(f"\nFinal spot = Column: {x}, Row: {y}")
 
+
 if __name__ == '__main__':
-
-    width = int(sys.argv[1])    # argument to create table - width
-    height = int(sys.argv[2])   # argument to create table - height
-    x = int(sys.argv[3])        # argument to place the ball - column
-    y = int(sys.argv[4])        # argument to place the ball -  row
-    direction = sys.argv[5]     # argument to set direction
-    steps = int(sys.argv[6])    # argument to determine count of steps
-
+    width = int(sys.argv[1])  # argument to create table - width
+    height = int(sys.argv[2])  # argument to create table - height
+    x = int(sys.argv[3])  # argument to place the ball - column
+    y = int(sys.argv[4])  # argument to place the ball -  row
+    direction = sys.argv[5]  # argument to set direction
+    steps = int(sys.argv[6])  # argument to determine count of steps
 
     table = create_table(width, height)
     placed_ball, start = place_ball(table, x, y)
     move_ball(placed_ball, start, direction, steps)
-
